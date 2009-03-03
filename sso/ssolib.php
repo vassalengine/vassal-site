9f11217b0fd695a2211c4ab3db8860886bfe3a2c
uckelman <uckelman@nomic.net>
2009-03-03 23:51:11 +0000

    Updated header.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@5207 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- sso/ssolib.php
+++ sso/ssolib.php
@@ -258,29 +258,22 @@ function print_top($title) {
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head profile="http://www.w3.org/2005/10/profile">
   <link rel="stylesheet" type="text/css" href="style.css"/>
+  <link rel="stylesheet" type="text/css" href="/site.css"/>
   <link rel="icon" type="image/png" href="VASSAL.png"/>
   <title>$title</title>
 </head>
 <body>
-<div id="vassal-page">
-  <div id="vassal-header">
-    <div id="vassal-logo">
-      <a href="/index.php"><img src="images/header.png"/></a>
-    </div>
+<div id="vassal-header">
+  <div id="vassal-logo">
+    <a href="/index.php"><img src="images/header.png"/></a>
   </div>
-  <div id="vassal-navigation">
-    <ul id="vassal-nav-list">
-      <li><a href="download.html">Download</a></li>
-      <li><a href="">Modules</a></li>
-      <li><a href="">FAQ</a></li>
-      <li><a href="">Documentation</a></li>
-      <li><a href="forum/">Forum</a></li>
-      <li><a href="news.php">News</a></li>
-      <li><a href="">Help</a></li>
-    </ul>
-  </div>
+END;
 
-  <div id="content">
+  virtual('/navigation.shtml');
+
+  echo <<<END
+</div>
+<div id="content">
 END;
 }
 
