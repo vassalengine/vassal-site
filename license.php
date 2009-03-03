4c36c98f920919e47b24ad2853c4fa378fed415a
uckelman <uckelman@nomic.net>
2009-03-03 13:14:20 +0000

    Added license page.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@5203 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ license.php
@@ -0,0 +1,47 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head profile="http://www.w3.org/2005/10/profile">
+  <link rel="stylesheet" type="text/css" href="/style.css"/>
+  <link rel="stylesheet" type="text/css" href="/site.css"/>
+  <link rel="icon" type="image/png" href="/images/VASSAL.png"/>
+  <title>VASSAL License</title>
+</head>
+<body>
+<div id="vassal-header">
+  <div id="vassal-logo">
+    <a href="/index.php"><img src="/images/header.png"/></a>
+  </div>
+  <?php virtual('/navigation.shtml'); ?>
+</div>
+
+<div id="content">
+<div class="content_box_full">
+  <h1><acronym>Vassal</acronym> License</h1>
+  <p><acronym>Vassal</acronym> is released under the <a href="http://creativecommons.org/licenses/LGPL/2.1/">GNU Lesser General Public License, version 2.1 or later</a>.</p>
+
+  <p>This programs free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.</p>
+
+  <p>This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.</p>
+
+   <p>You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA</p>
+
+  <h1>Website Content</h1>
+  <p>Website content (except game modules) is released under the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-Share Alike 3.0 Unported.</a> license.</p>
+
+  <p>All products mentioned on these pages are trademarks of their respective owners, where appropriate.</p>
+
+</div>
+</div>
+
+<?php virtual('/footer.shtml'); ?>
+</body>
+</html>
