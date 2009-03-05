03ebb5d550687b19efb4659102194aac30f97ea4
uckelman <uckelman@nomic.net>
2009-03-05 19:17:12 +0000

    Added stubs for missing pages.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@5219 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ help.php
@@ -0,0 +1,21 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head profile="http://www.w3.org/2005/10/profile">
+  <link rel="stylesheet" type="text/css" href="/style.css"/>
+  <link rel="stylesheet" type="text/css" href="/css/site.css"/>
+  <link rel="icon" type="image/png" href="/images/VASSAL.png"/>
+  <title>VASSAL Help</title>
+</head>
+<body>
+<?php include('inc/header.php'); ?>
+
+<div id="content">
+  <div id="content_box_full">
+    <h1>Help</h1>
+  </div>
+</div>
+
+<?php include('inc/footer.shtml'); ?>
+</body>
+</html>
---
+++ sitemap.php
@@ -0,0 +1,37 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head profile="http://www.w3.org/2005/10/profile">
+  <link rel="stylesheet" type="text/css" href="/style.css"/>
+  <link rel="stylesheet" type="text/css" href="/css/site.css"/>
+  <link rel="icon" type="image/png" href="/images/VASSAL.png"/>
+  <title>VASSAL Sitemap</title>
+</head>
+<body>
+<?php include('inc/header.php'); ?>
+
+<div id="content">
+  <div id="content_box_full">
+    <h1>Sitemap</h1>
+    <ul>
+      <li><a href="/about.php">About</a></li>
+      <li><a href="/documentation.php">Documentation</a></li>
+      <li><a href="/donate.php">Donate</a></li>
+      <li><a href="/download.php">Download</a></li>
+      <li><a href="/forum/">Forum</a></li>
+      <li><a href="/help.php">Help</a></li>
+      <li><a href="/index.php">Index</a></li>
+      <li><a href="/license.php">License</a></li>
+      <li><a href="">Modules</a></li>
+      <li><a href="/news.php">News</a></li>
+      <li><a href="/search.php">Search</a></li>
+      <li><a href="/sitemap.php">Sitemap</a></li>
+      <li><a href="/tracker/">Tracker</a></li>
+      <li><a href="/wiki/">Wiki</a></li>
+    </ul>
+  </div>
+</div>
+
+<?php include('inc/footer.shtml'); ?>
+</body>
+</html>
