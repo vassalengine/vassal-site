549932f70146a5dfd5d8dc9a96f3adb2b0002dd4
uckelman <uckelman@nomic.net>
2009-03-08 18:50:35 +0000

    Fixed spacing.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@5243 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- search.php
+++ search.php
@@ -8,6 +8,12 @@
   <title>VASSAL Search</title>
 
   <style type="text/css">
+@import url(http://www.google.com/cse/api/branding.css);
+
+#cse-search-results {
+  margin-top: 2em;
+}
+
 .search-block {
   margin: 1ex 0;
 }
@@ -20,7 +26,11 @@
   margin-bottom: -5pt;
 }
 
-#search {
+#search-outer {
+  margin: 3em 0;
+}
+
+#search-inner {
   margin: 0 auto;
   width: 45em;
 }
@@ -29,9 +39,6 @@
   overflow: visible;
 }
 
-#cse-search-results {
-  margin-top: 2em;
-}
 
   </style>
 </head>
@@ -39,16 +46,14 @@
 <?php include('inc/header.php'); ?>
 
 <div id="content">
-  <div id="content_box_full">
-    <h1>Search</h1>
-    <p>Looking for something? You can search our site in four ways: the whole site, only the <a href="/wiki">wiki</a>, only the <a href="/forum">forum</a>, or only the <a href="/tracker">tracker</a>. Additionally, each of the wiki, forum, and tracker provide an advanced search interface for special searches.</p>
+<div id="content_box_full">
+  <h1>Search</h1>
+  <p>Looking for something? You can search our site in four ways: the whole site, only the <a href="/wiki">wiki</a>, only the <a href="/forum">forum</a>, or only the <a href="/tracker">tracker</a>. Additionally, each of the wiki, forum, and tracker provide an advanced search interface for special searches.</p>
 
-    <div id="search">
+<div id="search-outer">
+  <div id="search-inner">
 <!-- search code from Google -->
     <div class="search-block" id="google-search">
-      <style type="text/css">
-        @import url(http://www.google.com/cse/api/branding.css);
-      </style>
       <div class="cse-branding-right" style="background-color:#FFFFFF;color:#000000">
         <div class="cse-branding-form">
           <form action="" id="cse-search-box-main">
@@ -72,40 +77,49 @@
 
 <!-- search code from MediaWiki -->
     <div class="search-block">
-      <form action="/wiki/index.php/Special:Search" id="searchform">
-        <input id="searchInput" name="search" type="text" size="31" />
-        <input type='submit' name="fulltext" value="Search Wiki" />
-        <a href="/wiki/index.php/Special:Search">Advanced search</a>
+      <form action="/wiki/index.php/Special:Search" method="get">
+        <div>
+          <input name="search" type="text" size="31" />
+          <input type='submit' name="fulltext" value="Search Wiki" />
+          <a href="/wiki/index.php/Special:Search">Advanced search</a>
+        </div>
       </form>
     </div>
   
 <!-- search code from phpBB -->
     <div class="search-block">
-      <form action="/forum/search.php" method="post" id="search">
-        <input name="keywords" id="keywords" type="text" maxlength="128" size="31" /> 
-        <input value="Search Forum" type="submit" />
-        <a href="/forum/search.php" title="View the advanced search options">Advanced search</a>
+      <form action="/forum/search.php" method="post">
+        <div>
+          <input name="keywords" type="text" maxlength="128" size="31" /> 
+          <input value="Search Forum" type="submit" />
+          <a href="/forum/search.php" title="View the advanced search options">Advanced search</a>
+        </div>
       </form>
     </div>
 
 <!-- search code from Bugzilla -->
     <div class="search-block">
       <form action="/tracker/buglist.cgi" method="get">
-        <input type="text" name="quicksearch" size="31" />
-        <input type="submit" value="Search Tracker" id="find_top" />
-        <a href="/tracker/query.cgi?format=advanced">Advanced search</a>
+        <div>
+          <input type="text" name="quicksearch" size="31" />
+          <input type="submit" value="Search Tracker" />
+          <a href="/tracker/query.cgi?format=advanced">Advanced search</a>
+        </div>
       </form>
     </div>
 
-    </div> 
+  </div> 
+</div>
 
     <div id="cse-search-results"></div>
     <script type="text/javascript">
+<!--
       var googleSearchIframeName = "cse-search-results";
       var googleSearchFormName = "cse-search-box-main";
       var googleSearchFrameWidth = 600;
       var googleSearchDomain = "www.google.com";
       var googleSearchPath = "/cse";
+-->
     </script>
     <script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
   </div>
