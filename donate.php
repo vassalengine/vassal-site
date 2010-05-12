420b1238efefae1c68ef7886f6e5b49cbe2f467e
uckelman <uckelman@nomic.net>
2010-05-12 20:14:18 +0000

    Cleanup.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@6862 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- donate.php
+++ donate.php
@@ -13,6 +13,17 @@
 <div id="content">
   <div id="content_box_full">
     <h1>Support <acronym>Vassal</acronym></h1>
+   
+    <p><acronym>Vassal</acronym> is free, open-source software, created by volunteers. While <acronym>Vassal</acronym> is free, development and hosting are not. If you enjoy using <acronym>Vassal</acronym>, please consider helping us with these costs.</p>
+
+    <div class="dlbutton">
+      <a target="_blank" href="https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&amp;SESSION=gS5hJbXNrYzfhrwcaHyrBsSqND22MpcGz07W_M7D94YX9wq1CFH4rbdTb7e&amp;dispatch=5885d80a13c0db1fa798f5a5f5ae42e779d4b5655493f6171509c5b2ec019b86"><span>Support <acronym>Vassal</acronym></span><img src="/images/paypal_donate.png" alt="" /></a>
+    </div>
+
+    <p>Have you ever wondered just how much effort we've put into <acronym>Vassal</acronym>? Have a look a this project cost calculator to get an idea:</p>
+    <div class="costbox">
+      <script type="text/javascript" src="http://www.ohloh.net/p/27263/widgets/project_cocomo.js"></script>
+    </div>
   </div>
 </div>
 
