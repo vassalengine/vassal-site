cdc52b284cfe20458a2fd93137aebd93d3fe2169
uckelman <uckelman@nomic.net>
2010-05-15 17:40:32 +0000

    Move style.css.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@6870 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- license.php
+++ license.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head profile="http://www.w3.org/2005/10/profile">
-  <link rel="stylesheet" type="text/css" href="/style.css"/>
+  <link rel="stylesheet" type="text/css" href="/css/style.css"/>
   <link rel="stylesheet" type="text/css" href="/css/site.css"/>
   <link rel="icon" type="image/png" href="/images/VASSAL.png"/>
   <title>VASSAL License</title>
