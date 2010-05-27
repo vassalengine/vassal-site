ca17601a92f50e4d38434354b6afcb2d8a545f26
uckelman <uckelman@nomic.net>
2010-05-27 13:48:22 +0000

    * Use phpns for news. * Moved style.css. * Added IE fix script.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@6910 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head profile="http://www.w3.org/2005/10/profile">
-  <link rel="stylesheet" type="text/css" href="/style.css"/>
+  <link rel="stylesheet" type="text/css" href="/css/style.css"/>
   <link rel="stylesheet" type="text/css" href="/css/site.css"/>
   <link rel="alternate" type="application/rss+xml" title="RSS" href="/news_rss.php" />
   <link rel="icon" type="image/png" href="/images/VASSAL.png"/>
   <title>VASSAL</title>
+
+  <!--[if lte IE 9]>
+    <script src="/js/IE9.js" type="text/javascript">IE7_PNG_SUFFIX=".png";</script>
+  <![endif]-->
 </head>
 <body>
 <?php include('inc/header.php'); ?>
@@ -44,6 +48,8 @@
           echo "<a href=\"$download_url\"><span>Download <acronym>Vassal</acronym><br/>$version$download_os</span><img src=\"/images/button.download.png\" alt=\"\" /></a>";
         ?>
       </div>
+      <p>Or, <a href="/download.php">download <acronym>Vassal</acronym></a> for other operating systems. 
+
       <div class="dlbutton">
         <a target="_blank" href="https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&amp;SESSION=gS5hJbXNrYzfhrwcaHyrBsSqND22MpcGz07W_M7D94YX9wq1CFH4rbdTb7e&amp;dispatch=5885d80a13c0db1fa798f5a5f5ae42e779d4b5655493f6171509c5b2ec019b86"><span>Support <acronym>Vassal</acronym></span><img src="/images/paypal_donate.png" alt="" /></a>
       </div>
@@ -55,48 +61,8 @@
     <div id="news">
       <h1>Latest News <a href="/news_rss.php"><img src="/images/feed-icon-14x14.png" alt="RSS feed"></a></h1>
 
-<?php
+      <?php include('inc/headlines.php'); ?>
 
-$rows = array();
-
-try {
-  require_once('sso/NewsDB.php');
-
-  $news = new NewsDB();
-  $limit = 10;
-  $query = "SELECT id, DATE_FORMAT(date,\"%b\") AS month, DAYOFMONTH(date) AS day, headline FROM news ORDER BY date DESC LIMIT $limit";
-  
-  $rows = $news->read_all($query);
-}
-catch (ErrorException $e) {
-  warn($e->getMessage());
-}
-
-if (count($rows) > 0) {
-  echo '<ul>';
-
-  $month = '';
-  $day = 0;
-
-  # print each item
-  foreach ($rows as $item) {
-    if ($day != $item['day'] || $month != $item['month']) {
-      if ($day != 0) echo '</ul></li>';
-
-      echo '<li class="day"><div class="date">',
-        $item['month'], '<br/>', $item['day'], '</div><ul class="events">';
-
-      $day = $item['day'];
-      $month = $item['month'];
-    }
-
-    echo '<li><a href="/news.php#_', $item['id'], '">',
-      $item['headline'], '</a></li>';
-  }
-
-  echo '</ul></li></ul>';
-}
-?>
       <div class="news-nav">
         <em><a href="/news.php">...more news</a></em>
       </div>
