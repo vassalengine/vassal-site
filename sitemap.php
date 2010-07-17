34ace6e1abbc7963cb68e0e2008d29987fee8db6
uckelman <uckelman@nomic.net>
2010-07-17 20:34:33 +0000

    Added Documentation and Modules links.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7000 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- sitemap.php
+++ sitemap.php
@@ -15,14 +15,14 @@
     <h1>Sitemap</h1>
     <ul>
       <li><a href="/about.php">About</a></li>
-      <li><a href="/documentation.php">Documentation</a></li>
+      <li><a href="/wiki/Category:Documentation">Documentation</a></li>
       <li><a href="/donate.php">Donate</a></li>
       <li><a href="/download.php">Download</a></li>
       <li><a href="/forum/">Forum</a></li>
       <li><a href="/help.php">Help</a></li>
       <li><a href="/index.php">Index</a></li>
       <li><a href="/license.php">License</a></li>
-      <li><a href="">Modules</a></li>
+      <li><a href="/wiki/Category:Modules">Modules</a></li>
       <li><a href="/news.php">News</a></li>
       <li><a href="/search.php">Search</a></li>
       <li><a href="/sitemap.php">Sitemap</a></li>
