a4f3cbc66a69bd75386522d2207e6f637518d872
uckelman <uckelman@nomic.net>
2010-07-25 20:25:48 +0000

    Updated for new domain.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7035 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- sso/ssolib.php
+++ sso/ssolib.php
@@ -244,7 +244,7 @@ function set_cookies($cookies) {
       $attr['value'],
       array_key_exists('expires', $attr) ? strtotime($attr['expires']) : 0,
       $attr['path'],
-      'www.test2.nomic.net',
+      'www.vassalengine.org',
       false,
       array_key_exists('httponly', $attr)
     );
