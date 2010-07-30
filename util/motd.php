aab926e5fe5f3fb2fe25a02c944caec122fde9fe
uckelman <uckelman@nomic.net>
2010-07-30 21:33:36 +0000

    Don't let file reads escape the motd_files directory.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7061 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- util/motd.php
+++ util/motd.php
@@ -1,13 +1,17 @@
 <?php
-$module=$_REQUEST['module'];
-$dir = "motd_files/";
-if (file_exists("$dir/$module")) {
-  $motdFile = "$dir/$module";  
-}
-else {
+
+$dir = '/var/www/html/util/motd_files';
+$module = isset($_REQUEST['module']) ? $_REQUEST['module'] : '';
+
+$motdFile = realpath("$dir/$module");
+
+# Make sure that the motd path does not take us above dir
+if ($motdFile === false ||
+    strncmp($motdFile, "$dir/", strlen($dir)+1) != 0 ||
+    !file_exists($motdFile)) {
   $motdFile = "$dir/VASSAL";
 }
-$fp = fopen($motdFile,"r");
-fpassthru($fp);
-fclose($fp);
+
+readfile($motdFile);
+
 ?>
