e5d56eba0596efc2659d50da031e8da1f6461175
uckelman <uckelman@nomic.net>
2010-07-31 14:15:32 +0000

    Cleanup. Rewritten to use improved table structure.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7064 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- util/updateConnections.php
+++ util/updateConnections.php
@@ -1,26 +1,56 @@
 <?php
-include_once "date.php";
 
-$now = getGmtTimeMillis() - 3600 * 8 * 1000;
-
-$STATUS=stripSlashes($_REQUEST['STATUS']);
-
-$connections = fopen("connections/connectionStatus","w");
-fwrite($connections,$STATUS);
-fclose($connections);
-
-$db = mysql_connect( "localhost");
-mysql_select_db("test");
-
-$line = strtok($STATUS,"\n");
-while ($line) {
-  list ($module, $game, $player) = split("\t",$line);
-  $module = mysql_real_escape_string($module);
-  $game = mysql_real_escape_string($game);
-  $player = mysql_real_escape_string($player);
-
-  mysql_query("DELETE FROM vassal_server_connections WHERE module_name = '$module' AND game_room = '$game' AND player_name = '$player'");
-  mysql_query("INSERT INTO vassal_server_connections (module_name, game_room, player_name, time) values ('$module', '$game', '$player', '$now')");
-  $line = strtok("\n");
+# bail if there's no input
+if (!isset($_REQUEST['STATUS'])) {
+  exit;
 }
+
+$now = time();
+
+# FIXME: what encoding for status? seems to contain garbage sometimes
+# FIXME: used to stripslashes(), was this necessary?
+$status = $_REQUEST['STATUS'];
+
+#$connections = fopen('connections/connectionStatus','w');
+#fwrite($connections,$STATUS);
+#fclose($connections);
+
+# connect to the SQL server
+require_once(dirname(__FILE__) . '/config.php');
+
+$dbh = mysql_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD);
+if (!$dbh) {
+  throw new ErrorException('Cannot connect to MySQL server: ' . mysql_error());
+}
+
+if (!mysql_select_db(SQL_DB, $dbh)) {
+  throw new ErrorException('Cannot select database: ' . mysql_error());
+}
+
+# parse the status data
+for ($line = strtok($status, "\n"); $line; $line = strtok("\n")) {
+  list($module, $game, $player) = explode("\t", $line, 3);
+
+  if (strpos($player, "\t") !== false) {
+    throw new ErrorException('Status line contains junk: ' . $line);
+  }
+
+  # map module-room-player triples to the current time
+  $query = sprintf(
+    'INSERT INTO connections (module_name, game_room, player_name, time) ' .
+    'VALUES ("%s", "%s", "%s", FROM_UNIXTIME(%d)) ' .
+    'ON DUPLICATE KEY UPDATE time = FROM_UNIXTIME(%d)',
+    mysql_real_escape_string($module),
+    mysql_real_escape_string($game),
+    mysql_real_escape_string($player),
+    $now,
+    $now
+  );
+
+  $r = mysql_query($query, $dbh);
+  if (!$r) {
+    throw new ErrorException('Replace failed: ' . mysql_error());
+  }
+}
+
 ?>
