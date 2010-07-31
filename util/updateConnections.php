de5347e0188254e81f71fa418a12b3620d6b5820
uckelman <uckelman@nomic.net>
2010-07-31 12:56:41 +0000

    Cleanup.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7063 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- util/updateConnections.php
+++ util/updateConnections.php
@@ -14,12 +14,13 @@ mysql_select_db("test");
 
 $line = strtok($STATUS,"\n");
 while ($line) {
-  list($module,$game,$player) = split("\t",$line);
+  list ($module, $game, $player) = split("\t",$line);
   $module = mysql_real_escape_string($module);
   $game = mysql_real_escape_string($game);
   $player = mysql_real_escape_string($player);
-  mysql_query("delete from vassal_server_connections where module_name = '$module' AND game_room = '$game' AND PLAYER_NAME = '$player'");
-  mysql_query("insert into vassal_server_connections(module_name,game_room,player_name,time) values ('$module','$game','$player','$now')");
+
+  mysql_query("DELETE FROM vassal_server_connections WHERE module_name = '$module' AND game_room = '$game' AND player_name = '$player'");
+  mysql_query("INSERT INTO vassal_server_connections (module_name, game_room, player_name, time) values ('$module', '$game', '$player', '$now')");
   $line = strtok("\n");
 }
 ?>
