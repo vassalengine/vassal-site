cf11f55e1d2796e9e0c9632b12c7ca0e9739c653
uckelman <uckelman@nomic.net>
2010-08-01 20:51:31 +0000

    Put messages into the database rather than on the filesystem.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7079 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- util/getMessages.php
+++ util/getMessages.php
@@ -1,31 +1,41 @@
 <?php
-include_once "date.php";
 
-$module=$_REQUEST['module'];
-if ($module) {
-  $module = str_replace("/","%2F",$module);
-  cleanMessages($module);
-  if (file_exists("msgs/$module")) {
-    $msgFile = fopen("msgs/$module","r");
-    fpassthru($msgFile);
-  }
+header('Content-type: text/plain; charset=utf-8');
+
+# validate input
+$module = isset($_REQUEST['module']) ? $_REQUEST['module'] : '';
+
+if (empty($module)) {
+  throw new ErrorException('no module specified');
 }
 
-function cleanMessages(&$module) {
-  $now = getGmtTimeMillis();
-  $expiration = $now - 14*3600*24*1000;
-  $filename = "msgs/$module";
-  if (is_writeable($filename)) {
-    $msgs = file($filename);
-    $msgFile = fopen("msgs/$module","w");
-    foreach ($msgs as $msg) {
-      $dateToEnd = strstr($msg,"DATE=");
-      $date = substr($dateToEnd,5,strpos($dateToEnd,"&")-5);
-      if ($date >= $expiration) {
-	fwrite($msgFile,$msg);
-      }
-    }
-    fclose($msgFile);
-  }
+# connect to the SQL server
+require_once(dirname(__FILE__) . '/config.php');
+
+$dbh = mysql_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD);
+if (!$dbh) {
+  throw new ErrorException('Cannot connect to MySQL server: ' . mysql_error());
 }
+
+if (!mysql_select_db(SQL_DB, $dbh)) {
+  throw new ErrorException('Cannot select database: ' . mysql_error());
+}
+
+# read messages for this module
+$query = sprintf(
+  'SELECT sender, UNIX_TIMESTAMP(time)*1000, content FROM messages ' .
+  'WHERE module_name = "%s"',
+  mysql_real_escape_string($module)
+);
+
+$r = mysql_query($query, $dbh);
+if (!$r) {
+  throw new ErrorException('SELECT failed: ' . mysql_error());
+}
+
+# send messages to the client
+while (($row = mysql_fetch_row($r))) {
+  echo vsprintf("SENDER=%s&DATE=%s&CONTENT=%s\n", $row);
+}
+
 ?>
--- util/postMessage.php
+++ util/postMessage.php
@@ -1,16 +1,47 @@
 <?php
-include_once "date.php";
 
-$module = $_REQUEST['module'];
-$content = stripslashes($_REQUEST['content']);
-$sender = $_REQUEST['sender'];
+$module = isset($_REQUEST['module']) ? $_REQUEST['module'] : '';
+$content = isset($_REQUEST['content']) ? $_REQUEST['content'] : '';
+$sender = isset($_REQUEST['sender']) ? $_REQUEST['sender'] : '';
 
-if ($module) {
-  $module = str_replace("/","%2F",$module);
-  $now = getGmtTimeMillis();
-  $msgFile = fopen("msgs/$module","a");
-  $msg = "SENDER=$sender&DATE=$now&CONTENT=$content\n";
-  fwrite($msgFile,$msg);
-  fclose($msgFile);
+if (empty($module)) {
+  throw new ErrorException('no module specified');
 }
+
+if (empty($content)) {
+  throw new ErrorException('no content specified');
+}
+
+if (empty($sender)) {
+  throw new ErrorException('no sender specified');
+}
+
+# FIXME: used to stripslashes() on content, was this necessary?
+
+# connect to the SQL server
+require_once(dirname(__FILE__) . '/config.php');
+
+$dbh = mysql_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD);
+if (!$dbh) {
+  throw new ErrorException('Cannot connect to MySQL server: ' . mysql_error());
+}
+
+if (!mysql_select_db(SQL_DB, $dbh)) {
+  throw new ErrorException('Cannot select database: ' . mysql_error());
+}
+
+# store the message
+$query = sprintf(
+  'INSERT INTO messages (module_name, sender, content) ' .
+  'VALUES ("%s", "%s", "%s")',
+  mysql_real_escape_string($module),
+  mysql_real_escape_string($sender),
+  mysql_real_escape_string($content)
+);
+
+$r = mysql_query($query, $dbh);
+if (!$r) {
+  throw new ErrorException('INSERT failed: ' . mysql_error());
+}
+
 ?>
