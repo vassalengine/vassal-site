b2d734dbc5cff1e16b2b7f2386c4f125385dfa46
uckelman <uckelman@nomic.net>
2010-08-01 18:35:10 +0000

    * Obsolete. Kept to keep 2.9.9 (and earier) working. * Send text/plain, not text/html.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7071 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- util/getServerAddress.php
+++ util/getServerAddress.php
@@ -1,15 +1,8 @@
 <?php
-$module=$_REQUEST['module'];
-$address="63.144.41.3:5050"; // Michael's new host
-//$address="204.13.13.17:5050"; // Matrix
-//if ($module == "Downtown") {
-//  $address="sandstein.ethz.ch:5050"; // Torsten
-//}
-//else if ($module == "TestMod") {
-//  $address="63.144.41.3:5050"; // Michael's new host
-//}
-//else {
-//  $address="63.144.41.3:5050"; // Michael's new host
-//}
-echo $address;
+# 
+# This script is obsolete. Used by VASSAL 2.9.9 and earlier.
+# VASSAL 3.0 and later use getServerImpl.php instead.
+# 
+header('Content-type: text/plain; charset=utf-8');
+echo '66.253.49.166';
 ?>
