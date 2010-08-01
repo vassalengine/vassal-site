07e63d7471908ed328bb07d490351b3c1cc05170
uckelman <uckelman@nomic.net>
2010-08-01 17:31:17 +0000

    Complete rewrite.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7069 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- util/getConnectionHistory.php
+++ util/getConnectionHistory.php
@@ -1,36 +1,68 @@
 <?php
-include_once "date.php";
-$start=$_REQUEST['start'];
-$end=$_REQUEST['end'];
 
-$db = mysql_connect( "localhost");
-mysql_select_db("test");
+header('Content-type: text/plain; charset=utf-8');
 
-$touchFile = "historyCleanup";
-if (file_exists($touchFile)) {
-  $nextClean = filemtime($touchFile) + 3600*24;
-  if (time() > $nextClean) {
-    $now = getGmtTimeMillis();
-    $expiration = $now - 30*24*3600*1000;
-    mysql_query("delete from vassal_server_connections where time < '$expiration'");
+# validate input
+$start = isset($_REQUEST['start']) ? $_REQUEST['start'] : null;
+$end = isset($_REQUEST['end']) ? $_REQUEST['end'] : null;
+
+# either both start and end must be set, or neither
+if ($start !== null || $end !== null) {
+  
+  if (!is_numeric($start) || $start != (int) $start) {
+    throw new ErrorException('start is not an integer');
+  }
+
+  if (!is_numeric($end) || $end != (int) $end) {
+    throw new ErrorException('end is not an integer');
+  }
+
+  # start, end are in milliseconds, timestamps are in seconds, so we convert
+  $start = (int)($start/1000);
+  $end = (int)($end/1000);
+
+  if ($start < 0) {
+    throw new ErrorException('start is negative');
+  }
+
+  if ($end <= $start) {
+    throw new ErrorException('end <= start');
   }
 }
-else {
-  touch ($touchFile);
+
+# connect to the SQL server
+require_once(dirname(__FILE__) . '/config.php');
+
+$dbh = mysql_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD);
+if (!$dbh) {
+  throw new ErrorException('Cannot connect to MySQL server: ' . mysql_error());
 }
 
-if ($start && $end) {
-  $start = mysql_real_escape_string($start);
-  $end = mysql_real_escape_string($end);
-
-  $cur = mysql_query("select module_name,game_room, player_name,time from vassal_server_connections where time >= $start and time < $end order by time");
-}
-else {
-  $cur = mysql_query("select module_name,game_room, player_name,time from vassal_server_connections order by time");
+if (!mysql_select_db(SQL_DB, $dbh)) {
+  throw new ErrorException('Cannot select database: ' . mysql_error());
 }
 
-if ($cur) {
-  while ($row = mysql_fetch_row( $cur )) {
-    echo $row[0]."\t".$row[1]."\t".$row[2]."\t".$row[3]."\n";
-  }
+# build the query
+$query = 'SELECT module_name, game_room, player_name, UNIX_TIMESTAMP(time)*1000 FROM connections';
+
+if ($start !== null) {
+  # add the time interval, if we have one
+  $query .= sprintf(
+    ' WHERE time BETWEEN FROM_UNIXTIME(%s) AND FROM_UNIXTIME(%s)',
+    $start,
+    # BETWEEN wants [start,end], we use [start,end), so adjust end
+    $end + 1
+  );  
 }
+
+# get the rows and send them
+$r = mysql_query($query, $dbh);
+if (!$r) {
+  throw new ErrorException('SELECT failed: ' . mysql_error());
+}
+
+while (($row = mysql_fetch_row($r))) {
+  echo implode("\t", $row), "\n";
+}
+
+?>
