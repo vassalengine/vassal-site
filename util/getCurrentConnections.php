1cd91502794390369d40337d900e57613b79c984
uckelman <uckelman@nomic.net>
2010-08-01 12:00:20 +0000

    Get results directly from the database.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7067 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- util/getCurrentConnections.php
+++ util/getCurrentConnections.php
@@ -1,5 +1,36 @@
 <?php
-$connections = fopen("connections/connectionStatus","r");
-fpassthru($connections);
-fclose($connections);
+
+# connect to the SQL server
+require_once(dirname(__FILE__) . '/config.php');
+
+$dbh = mysql_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD);
+if (!$dbh) {
+  throw new ErrorException('Cannot connect to MySQL server: ' . mysql_error());
+}
+
+if (!mysql_select_db(SQL_DB, $dbh)) {
+  throw new ErrorException('Cannot select database: ' . mysql_error());
+}
+
+# get the set of rows having the most recent timestamp
+$query = 'SELECT module_name, game_room, player_name FROM connections ' .
+         'WHERE time = (SELECT MAX(time) FROM connections)';
+
+# TODO: Determine whether this join is faster than the subquery
+#$query = 'SELECT c1.module_name, c1.game_room, c1.player_name '
+#         'FROM connections AS c1 LEFT OUTER JOIN connections AS c2 ON '
+#         'c1.time < c2.time '
+#         'WHERE c2.module_name IS NULL';
+
+$r = mysql_query($query, $dbh);
+if (!$r) {
+  throw new ErrorException('SELECT failed: ' . mysql_error());
+}
+
+# header('Content-type: text/html; charset=utf-8');
+
+while (($row = mysql_fetch_row($r)) {
+  echo implode("\t", $row), "\n";
+}
+
 ?>
