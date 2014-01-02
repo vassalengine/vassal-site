42778dfb827cacdada9a70fd9347f816e01d5de4
Joel Uckelman <uckelman@nomic.net>
2014-01-02 13:13:00 -0700

    Updated Donate button.

--- index.php
+++ index.php
@@ -53,11 +53,11 @@
         <form id="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post">
           <button type="submit" name="submit">
             <span>Support <acronym>Vassal</acronym></span>
-            <img src="https://www.paypal.com/en_GB/i/btn/btn_donate_LG.gif" alt="Donate"/>
+            <img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="Donate"/>
           </button>
           <input type="hidden" name="cmd" value="_s-xclick"/>
-          <input type="hidden" name="hosted_button_id" value="Y584SZ4XBMGW6"/>
-          <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+          <input type="hidden" name="hosted_button_id" value="UQ943LRKHH7DL">
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
         </form>
       </div>
 
