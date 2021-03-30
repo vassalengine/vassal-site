a2e5ecab6dd15bdb5be94bad95af9a40fe0cd90f
Joel Uckelman <uckelman@nomic.net>
2021-03-31 00:21:08 +0100

    Added "Show all" link.

--- builds/artifacts.py
+++ builds/artifacts.py
@@ -1,5 +1,6 @@
 #!/bin/python3 
 
+import math
 import traceback
 
 from flask import Flask, app, redirect, request, render_template
@@ -10,12 +11,13 @@ import requests
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
-per_page = app.config['PER_PAGE']
-token = app.config['TOKEN']
-user = app.config['USER']
-repo = app.config['REPO']
-#api_url = f'https://api.github.com/repos/{user}/{repo}/actions/artifacts'
-api_url = 'https://api.github.com/repos/{user}/{repo}/actions/artifacts'.format(user=user, repo=repo)
+PER_PAGE = app.config['PER_PAGE']
+TOKEN = app.config['TOKEN']
+USER = app.config['USER']
+REPO = app.config['REPO']
+
+#API_URL = f'https://api.github.com/repos/{USER}/{REPO}/actions/artifacts'
+API_URL = 'https://api.github.com/repos/{USER}/{REPO}/actions/artifacts'.format(USER=USER, REPO=REPO)
 
 
 @app.errorhandler(Exception)
@@ -24,38 +26,65 @@ def handle_error(err):
     return render_template('error.html', err=tb), 500
 
 
+def get_page(page, per_page):
+    return requests.get(
+        API_URL,
+        params={'per_page': per_page, 'page': page},
+        headers={
+#            'Authorization': f'token {TOKEN}'
+            'Authorization': 'token ' + TOKEN,
+            'Accept': 'application/vnd.github.v3+json'
+        }
+    ).json()
+
+
 @app.route('/')
 @app.route('/list')
 def show_builds():
-    page = int(request.args.get('page', 1))
+    page = request.args.get('page', 1)
 
-    r = requests.get(
-        api_url,
-        params={'per_page': per_page, 'page': page},
-#        headers={'Authorization': f'token {token}'}
-        headers={'Authorization': 'token ' + token}
-    )
+    if page == 'all':
+        j = get_page(1, PER_PAGE)
+        total_count = j['total_count']
+        items = j['artifacts']
 
-    j = r.json()
-    total_count = j['total_count']
-    items = j['artifacts']
+        for p in range(2, math.ceil(total_count / PER_PAGE)):
+            items += get_page(p, PER_PAGE)['artifacts']
 
-    return render_template(
-        'list.html',
-        items=items,
-        page=page,
-        per_page=per_page,
-        total_count=total_count
-    )
+        return render_template(
+            'list.html',
+            items=items,
+            page=1,
+            per_page=total_count,
+            total_count=total_count
+        )
+
+    else:
+        page = int(page)
+        j = get_page(page, PER_PAGE)
+        total_count = j['total_count']
+        items = j['artifacts']
+
+        return render_template(
+            'list.html',
+            items=items,
+            page=page,
+            per_page=PER_PAGE,
+            total_count=total_count
+        )
 
 
 @app.route('/build/<build_id>')
 def request_build(build_id):
     r = requests.get(
-#        f'{api_url}/{build_id}/zip',
-#        headers={'Authorization': f'token {token}'},
-        '{api_url}/{build_id}/zip'.format(api_url=api_url, build_id=build_id),
-        headers={'Authorization': 'token ' + token},
+#        f'{API_URL}/{build_id}/zip',
+#        headers={'Authorization': f'token {TOKEN}'},
+        '{API_URL}/{build_id}/zip'.format(API_URL=API_URL, build_id=build_id),
+        headers={
+#            'Authorization': f'token {TOKEN}'
+            'Authorization': 'token ' + TOKEN,
+            'Accept': 'application/vnd.github.v3+json'
+        },
         allow_redirects=False
     )
     return redirect(r.headers['Location'])
