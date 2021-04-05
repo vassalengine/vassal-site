acd1256a770e515538b71f3822b2d710c434a9de
Joel Uckelman <uckelman@nomic.net>
2021-04-05 20:58:13 +0100

    Fixed typo.

--- license.php
+++ license.php
@@ -12,7 +12,7 @@
   <h1><abbr>Vassal</abbr> License</h1>
   <p><abbr>Vassal</abbr> is released under the <a href="http://creativecommons.org/licenses/LGPL/2.1/">GNU Lesser General Public License, version 2.1 or later</a>.</p>
 
-  <p>This programs free software; you can redistribute it and/or
+  <p>This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) any later version.</p>
