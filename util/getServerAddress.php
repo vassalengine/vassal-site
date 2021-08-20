3486d1bee5c04e40f0380b22099406279d6b52d6
Joel Uckelman <uckelman@nomic.net>
2021-08-20 20:28:57 +0000

    Removed cruft. These scripts are all long obsolete.

--- util/getServerAddress.php
+++
@@ -1,8 +0,0 @@
-<?php
-# 
-# This script is obsolete. Used by VASSAL 2.9.9 and earlier.
-# VASSAL 3.0 and later use getServerImpl.php instead.
-# 
-header('Content-type: text/plain; charset=utf-8');
-echo '62.210.178.7:5050';
-?>
