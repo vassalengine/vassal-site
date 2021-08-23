f00ffa2303a90be0559c02c8b41038b71b41371f
Joel Uckelman <uckelman@nomic.net>
2021-08-23 20:36:17 +0000

    Added news fetcher.

---
+++ js/news.js
@@ -0,0 +1,41 @@
+const itemTemplate = document.querySelector('#news_item_tmpl');
+
+function makeNewsItem(slug, id, title, month, day, base_url) {
+  const item = document.importNode(itemTemplate.content, true);
+
+  const m = item.querySelector('.tmpl-month');
+  m.append(month);
+
+  const d = item.querySelector('.tmpl-day');
+  d.append(day);
+
+  const a = item.querySelector('.tmpl-link');
+  a.setAttribute('href', `${base_url}/${slug}/${id}`);
+  a.append(title);
+
+  return item;
+}
+
+async function loadNews(news_url, base_item_url, count) {
+  const news = await (await fetch(news_url)).json();
+  const news_root = document.getElementById('news_root');
+
+  // The first topic is the "About" topic, skip it
+  news['topic_list']['topics'].slice(1, count + 1).map(t => {
+    const date = new Date(t['created_at']);
+    news_root.appendChild(makeNewsItem(
+      t['slug'],
+      t['id'],
+      t['title'],
+      date.toLocaleString('en', {month: 'short'}),
+      date.toLocaleString('en', {day: 'numeric'}),
+      base_item_url
+    ));
+  });
+}
+
+const news_url = 'https://www.test.vassalengine.org/forum/c/news/18.json';
+const base_item_url = '/forum/t';
+const count = 10;
+
+loadNews(news_url, base_item_url, count);
