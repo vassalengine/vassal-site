b05de0528711a62a81d35492b7f4c3462cbfb5af
Joel Uckelman <uckelman@nomic.net>
2022-02-01 23:39:26 +0000

    Added OS and CPU detection for downloads.

---
+++ js/detect.js
@@ -0,0 +1,189 @@
+async function try_navigator_userAgentData() {
+  let uach = null;
+  if (navigator.userAgentData) {
+    // this browser supports userAgentData
+    const uach = await navigator.userAgentData.getHighEntropyValues(['architecture', 'bitness', 'platform']);
+  
+    // fill in platform if we need to (do we ever?) 
+    if (!uach.platform) {
+      uach.platform = navigator.userAgentData.platform;
+    }
+  }
+  return uach;
+}
+
+function has_m1_gpu() {
+  let gl = null;
+  const canvas = document.createElement("canvas");
+  try {
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  }
+  catch (e) {
+    return false;
+  }
+
+  if (!gl) {
+    return false;
+  }
+
+  const d = gl.getExtension('WEBGL_debug_renderer_info');
+  const r = gl.getParameter(d.UNMASKED_RENDERER_WEBGL);
+  return r === 'Apple M1';
+}
+
+const PLATFORM_MACOS = 'macOS';
+const PLATFORM_WINDOWS = 'Windows';
+const PLATFORM_LINUX = 'Linux';
+
+const ARCH_X86 = 'x86';
+const ARCH_ARM = 'arm';
+
+const BITS_32 = '32';
+const BITS_64 = '64';
+
+async function get_userAgentData() {
+  // get userAgentData if available
+  const uach = await try_navigator_userAgentData() || {};
+
+  // try to fill in userAgentData if anything we need is unknown
+  if (!uach.platform || !uach.architecture || !uach.bitness) {
+    const parser = new UAParser();
+
+    if (!uach.platform) {
+      const os = parser.getOS();
+
+      if (os.name === 'Mac OS') {
+        uach.platform = PLATFORM_MACOS;
+      }
+      else if (os.name === 'Windows') {
+        uach.platform = PLATFORM_WINDOWS;
+      }
+      else if (parser.getUA().includes('Linux')) {
+        // getOS() returns distro names, not Linux
+        uach.platform = PLATFORM_LINUX;
+      }
+    }
+
+    if (!uach.architecture || !uach.bitness) {
+      if (uach.platform === PLATFORM_MACOS) {
+        // getCPU() cannot distinguish x86 from ARM Macs 
+        if (has_m1_gpu()) {
+          uach.architecture = ARCH_ARM;
+          uach.bitness = BITS_64;
+        }
+        else {
+          uach.architecture = ARCH_X86;
+          uach.bitness = BITS_64;
+        }
+      }
+      else {
+        const arch = parser.getCPU().architecture;
+
+        if (arch === 'amd64') {
+          uach.architecture = ARCH_X86;
+          uach.bitness = BITS_64;
+        }
+        else if (arch === 'arm64') {
+          uach.architecture = ARCH_ARM;
+          uach.bitness = BITS_64;
+        }
+      }
+    }
+  }
+
+  return uach;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+  // get what data we can
+  const uach = await get_userAgentData();
+
+  // now match what we care about for releases
+  let os;
+  switch (uach.platform) {
+  case PLATFORM_LINUX:
+  case PLATFORM_MACOS:
+  case PLATFORM_WINDOWS:
+    os = uach.platform;
+    break;
+  default:
+    os = 'unknown';
+    break;
+  }
+
+  let cpu = 'unknown';
+  switch (uach.architecture) {
+  case ARCH_X86:
+    if (uach.bitness === BITS_64 || uach.bitness === BITS_32) {
+      cpu = `x86_${uach.bitness}`;
+    }
+    break;
+  case ARCH_ARM:
+    if (uach.bitness === BITS_64) {
+      cpu = 'aarch64';
+    }
+    break;
+  }
+
+  document.getElementById('os_' + os).classList.add('found');
+  document.getElementById('cpu_' + cpu).classList.add('found');
+
+  const base_url = 'https://github.com/vassalengine/vassal/releases';
+
+  const ver = '3.6.4';
+  const dl_url = `${base_url}/download/${ver}`;
+
+  const get_vassal = 'Get Vassal';
+  let btn_text = get_vassal;
+  let btn_link = `${base_url}/latest`;
+
+  let specific_download = false;
+
+  if (uach.platform === PLATFORM_WINDOWS) {
+    if (uach.architecture === ARCH_X86) {
+      if (uach.bitness === BITS_64) {
+        specific_download = true;
+        btn_text = `${get_vassal} for ${uach.platform} (64-bit x86)`;
+        btn_link = `${dl_url}/VASSAL-${ver}-windows-x86_64.exe`;
+      }
+      else if (uach.bitness === BITS_32) {
+        specific_download = true;
+        btn_text = `${get_vassal} for ${uach.platform} (32-bit x86)`;
+        btn_link = `${dl_url}/VASSAL-${ver}-windows-x86_32.exe`;
+      }
+    }
+    else if (uach.architecture === ARCH_ARM && uach.bitness === BITS_64) {
+      specific_download = true;
+      btn_text = `${get_vassal} for ${uach.platform} (64-bit ARM)`;
+      btn_link = `${dl_url}/VASSAL-${ver}-windows-aarch64.exe`;
+    }
+  }
+  else if (uach.platform === PLATFORM_MACOS && uach.bitness === BITS_64) {
+    if (uach.architecture === ARCH_X86) {
+      specific_download = true;
+      btn_text = `${get_vassal} for ${uach.platform} (Intel)`;
+      btn_link = `${dl_url}/VASSAL-${ver}-macos-x86_64.dmg`;
+    }
+    else if (uach.architecture === ARCH_ARM) {
+      specific_download = true;
+      btn_text = `${get_vassal} for ${uach.platform} (Apple Silicon)`;
+      btn_link = `${dl_url}/VASSAL-${ver}-macos-aarch64.dmg`;
+    }
+  }
+  else if (uach.platform === PLATFORM_LINUX) {
+    specific_download = true;
+    btn_text = `${get_vassal} for ${uach.platform}`;
+    btn_link = `${dl_url}/VASSAL-${ver}-linux.tar.bz2`;
+  }
+
+  if (!specific_download) {
+    // detection failed to be specific enough
+    const alt_downloads = document.getElementById('alt_downloads');
+    alt_downloads.style.display = 'none';
+  }
+
+  const btn = document.getElementById('download_btn');
+  btn.textContent = btn_text; 
+  btn.href = btn_link;
+});
