d0c9dc5ff6a1468fd7ba1c01678ad5596d2ec820
Joel Uckelman <uckelman@nomic.net>
2023-03-02 15:56:26 +0000

    Updated for 3.6.14 release.

--- status.php
+++ status.php
@@ -44,7 +44,7 @@
   </header>
 
   <div id="alert-bar" class="text-center">
-    <h5 class="py-3">Vassal 3.6.13 is released! See the <a class="link-light" href="https://forum.vassalengine.org/t/vassal-3-6-13-released/76613">news</a> for details.</h5>
+    <h5 class="py-3">Vassal 3.6.14 is released! See the <a class="link-light" href="https://forum.vassalengine.org/t/vassal-3-6-14-released/76653">news</a> for details.</h5>
   </div>
 
   
