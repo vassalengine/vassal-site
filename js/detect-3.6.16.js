d2a4ab74b3819b789fed3dd14e8b7a451263b84b
Joel Uckelman <uckelman@nomic.net>
2023-04-17 22:40:47 +0100

    Updated for 3.6.16 release.

--- js/detect-3.6.16.js
+++ js/detect-3.6.16.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const base_url = 'https://github.com/vassalengine/vassal/releases';
 
-  const ver = '3.6.15';
+  const ver = '3.6.16';
   const dl_url = `${base_url}/download/${ver}`;
 
   const get_vassal = 'Get Vassal';
--- status.php
+++ status.php
@@ -44,7 +44,7 @@
   </header>
 
   <div id="alert-bar" class="text-center">
-    <h5 class="py-3">Vassal 3.6.15 is released! See the <a class="link-light" href="https://forum.vassalengine.org/t/vassal-3-6-15-released/76851">news</a> for details.</h5>
+    <h5 class="py-3">Vassal 3.6.16 is released! See the <a class="link-light" href="https://forum.vassalengine.org/t/vassal-3-6-16-released/76951">news</a> for details.</h5>
   </div>
 
   
