d028970b0d50aef5e838148b8a7a6f7f51ec81b2
Joel Uckelman <uckelman@nomic.net>
2023-10-16 22:21:48 +0100

    Updated for 3.7.3 release.

--- js/detect-3.7.3.js
+++ js/detect-3.7.3.js
@@ -91,7 +91,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const base_url = 'https://github.com/vassalengine/vassal/releases';
 
-  const ver = '3.7.2';
+  const ver = '3.7.3';
   const dl_url = `${base_url}/download/${ver}`;
 
   const get_vassal = 'Get Vassal';
