2b92b05735736d48f69c1d01209b148929663709
Joel Uckelman <uckelman@nomic.net>
2026-07-29 23:52:29 +0100

    Updated for 3.7.26 release.

--- js/detect-3.7.26.js
+++ js/detect-3.7.26.js
@@ -91,7 +91,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const base_url = 'https://github.com/vassalengine/vassal/releases';
 
-  const ver = '3.7.25';
+  const ver = '3.7.26';
   const dl_url = `${base_url}/download/${ver}`;
 
   const get_vassal = 'Get Vassal';
